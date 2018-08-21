74aa4e870f6ef23bcb2067e3ded01fc3a24c6a7a
CY Chen <cchen140@illinois.edu>
2018-08-21 08:34:11 -0500

    Fixed a minor bug that uses an incorrect constructor.

--- src/main/java/synercys/rts/cli/MainSingleTaskSetTest.java
+++ src/main/java/synercys/rts/cli/MainSingleTaskSetTest.java
@@ -72,8 +72,7 @@ public class MainSingleTaskSetTest {
         loggerConsole.info("Task Hyper-period: " + taskSetHyperPeriod);
 
         // New and configure a RM scheduling simulator.
-        QuickFixedPrioritySchedulerSimulator rmSimulator = new QuickFixedPrioritySchedulerSimulator();
-        rmSimulator.setTaskSet(taskSet);
+        QuickFixedPrioritySchedulerSimulator rmSimulator = new QuickFixedPrioritySchedulerSimulator(taskSet);
 
         //for (int i=1; i<=3; i++) {
         //    taskSet.getOneTaskByPriority(2+i).setSporadicTask(true);
