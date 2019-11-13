339cad4073bdfc27b61aece70a16acab29ace066
CY Chen <cchen140@illinois.edu>
2019-11-13 12:42:59 -0600

    [rtenc] Update experiment data for the entropy correlation test case.

--- experiments/test_cases/entropy_correlation/run_all.sh
+++ experiments/test_cases/entropy_correlation/run_all.sh
@@ -5,3 +5,4 @@ TASKSET_FOLDER="../../tasksets/smallHP_rmSchedulable"
 OUTPUT_FOLDER="."
 
 $BIN_FOLDER/rtenc -i $TASKSET_FOLDER/smallHP.tasksets -o $OUTPUT_FOLDER/correlation_rm_shannon -r 1000000 -p taskshuffler -e shannon -c FULL_HP
+$BIN_FOLDER/rtenc -i $TASKSET_FOLDER/smallHP.tasksets -o $OUTPUT_FOLDER/correlation_rm_apen -r 1000000 -p taskshuffler -e uapen -c FULL_HP
