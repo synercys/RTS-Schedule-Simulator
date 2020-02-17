57b333e964f582da5680fc1b527bd4e864a0a8ea
CY Chen <cchen140@illinois.edu>
2020-02-17 15:15:29 -0600

    [rtsim] Implement a class to handle instantiation of a scheduler

---
+++ src/main/java/synercys/rts/scheduler/SchedulerUtil.java
@@ -0,0 +1,49 @@
+package synercys.rts.scheduler;
+
+import cy.utility.Class;
+import synercys.rts.framework.TaskSet;
+
+import java.util.ArrayList;
+
+public class SchedulerUtil {
+    public static String SCHEDULER_RM = "RM";
+    public static String SCHEDULER_EDF = "EDF";
+    public static String SCHEDULER_TASKSHUFFLER = "TaskShuffler";
+    public static String SCHEDULER_REORDER = "ReOrder";
+
+    public static AdvanceableSchedulerInterface getScheduler(String schedulingPolicy, TaskSet taskSet, boolean executionVariation) {
+        if (schedulingPolicy.equalsIgnoreCase("RM") || schedulingPolicy.equalsIgnoreCase("TaskShuffler0"))
+            return new FixedPriorityScheduler(taskSet, executionVariation);
+        else if (schedulingPolicy.equalsIgnoreCase("EDF") || schedulingPolicy.equalsIgnoreCase("ReOrder0"))
+            return new EdfScheduler(taskSet, executionVariation);
+        else if (schedulingPolicy.substring(0,"ReOrder".length()).equalsIgnoreCase("ReOrder")) {  // ReOrder1, 2, 3, 4
+            String randomizationLevelStr = schedulingPolicy.substring("ReOrder".length());
+            int randomizationLevel;
+            if (randomizationLevelStr.equalsIgnoreCase("")) {
+                randomizationLevel = 4;
+            } else {
+                randomizationLevel = Integer.valueOf(randomizationLevelStr);
+            }
+            ReorderScheduler scheduler = new ReorderScheduler(taskSet, executionVariation);
+            scheduler.setRandomizationLevel(randomizationLevel);
+            return scheduler;
+        } else if (schedulingPolicy.substring(0,"TaskShuffler".length()).equalsIgnoreCase("TaskShuffler")) {  // TaskShuffler1, 2, 3, 4
+            String randomizationLevelStr = schedulingPolicy.substring("TaskShuffler".length());
+            int randomizationLevel;
+            if (randomizationLevelStr.equalsIgnoreCase("")) {
+                randomizationLevel = 4;
+            } else {
+                randomizationLevel = Integer.valueOf(randomizationLevelStr);
+            }
+            TaskShufflerScheduler scheduler = new TaskShufflerScheduler(taskSet, executionVariation);
+            scheduler.setRandomizationLevel(randomizationLevel);
+            return scheduler;
+        } else  // Use RM by default.
+            return new FixedPriorityScheduler(taskSet, executionVariation);
+    }
+
+    static public ArrayList<String> getSchedulerNames() {
+        // This function is from cy.utility
+        return Class.getPrefixMatchedVariableStringValues(SchedulerUtil.class, "SCHEDULER_");
+    }
+}
