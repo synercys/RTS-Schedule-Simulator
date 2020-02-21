49baaf41f0b03095d09979b9c9469be7db2f7565
CY Chen <cchen140@illinois.edu>
2020-02-21 17:09:30 -0600

    [rta] Break FFT analysis process into multiple function calls

--- src/main/java/synercys/rts/analysis/dft/ScheduleDFTAnalyzer.java
+++ src/main/java/synercys/rts/analysis/dft/ScheduleDFTAnalyzer.java
@@ -4,51 +4,74 @@ import org.apache.commons.math3.complex.Complex;
 import org.apache.commons.math3.transform.DftNormalization;
 import org.apache.commons.math3.transform.FastFourierTransformer;
 import org.apache.commons.math3.transform.TransformType;
-import synercys.rts.framework.event.BusyIntervalEventContainer;
 import synercys.rts.framework.event.EventContainer;
+
+import static synercys.rts.RtsConfig.TIMESTAMP_UNIT_TO_S_MULTIPLIER;
 
 
 public class ScheduleDFTAnalyzer {
+    static int DFT_DATA_PADDING_MODE_NEXT_POWER_OF_TWO = 0;
+    static int DFT_DATA_PADDING_MODE_LAST_POWER_OF_TWO = 1;
 
-    public double[] getFrequencySpectrumOfSchedule(EventContainer schedule) {
-        return transformFFTPowerOfTwo(schedule.toBinaryScheduleDouble(), 0);
+    // configurations
+    int paddingMode = DFT_DATA_PADDING_MODE_LAST_POWER_OF_TWO;
+
+    double[] binarySchedule = null;
+    double[] spectrum = null;
+
+
+    public void setBinarySchedule(EventContainer schedule) {
+        binarySchedule = schedule.toBinaryScheduleDouble();
+        validateBinarySchedule(-1);
     }
 
 
-    /**
-     *
-     * @param data raw data points to be analyzed by FFT
-     * @param admittedDataLength desired minimum length for the data to be analyzed;
-     *                           a value <=0 indicates that data.length will be used;
-     *                           the actual length of data to be analyzed will be a power of 2 value greater than
-     *                           this value (or data.length in the case of admittedDataLength <= 0)
-     * @return a double array representing the resulting frequency spectrum
-     */
-    public double[] transformFFTPowerOfTwo(double[] data, int admittedDataLength) {
-
+    protected void validateBinarySchedule(int admittedDataLength) {
         // determine the actual length that will be used for FFT computation
         // (the length must be in power of two)
         if (admittedDataLength <= 0) {
-            admittedDataLength = data.length;
+            admittedDataLength = binarySchedule.length;
         }
-        int actualPowerOfTwoLength = getNextPowerOfTwoInclusive(admittedDataLength);
-        // int actualPowerOfTwoLength = getLastPowerOfTwoInclusive(admittedDataLength);
+
+        int actualPowerOfTwoLength = computeValidDataLength(admittedDataLength);
 
         // Add padding zeros
         double[] zeroPaddedData = new double[actualPowerOfTwoLength];
-        System.arraycopy(data, 0, zeroPaddedData, 0, Math.min(actualPowerOfTwoLength, data.length));
+        System.arraycopy(binarySchedule, 0, zeroPaddedData, 0, Math.min(actualPowerOfTwoLength, binarySchedule.length));
 
+        binarySchedule = zeroPaddedData;
+    }
+
+    public double[] computeFreqSpectrum() {
         // Transform
         FastFourierTransformer transformer = new FastFourierTransformer(DftNormalization.STANDARD);
-        Complex[] fftComplexArray = transformer.transform(zeroPaddedData, TransformType.FORWARD);
+        Complex[] fftComplexArray = transformer.transform(binarySchedule, TransformType.FORWARD);
 
-        double spectrum[] = new double[(fftComplexArray.length/2)+1];
+        spectrum = new double[(fftComplexArray.length/2)+1];
         for (int i=0; i<(fftComplexArray.length/2)+1; i++) {
             // spectrum[i] = fftComplexArray[i].abs() * fftComplexArray[i].abs();
             spectrum[i] = fftComplexArray[i].abs() * fftComplexArray[i].abs() + fftComplexArray[i].getImaginary() * fftComplexArray[i].getImaginary();
         }
 
         return spectrum;
+    }
+
+
+    protected int computeValidDataLength(int rawLength) {
+        if (paddingMode == DFT_DATA_PADDING_MODE_NEXT_POWER_OF_TWO) {
+            return getNextPowerOfTwoInclusive(rawLength);
+        } else {
+            return getLastPowerOfTwoInclusive(rawLength);
+        }
+    }
+
+
+    public int getAnalyzedDataLength() {
+        if (binarySchedule == null) {
+            return 0;
+        } else {
+            return binarySchedule.length;
+        }
     }
 
 
@@ -64,5 +87,29 @@ public class ScheduleDFTAnalyzer {
 
     static public int getLastPowerOfTwoInclusive(int value) {
         return Math.max(1, Integer.highestOneBit(value));
+    }
+
+
+    public double getPeakFreq() {
+        if (spectrum == null) {
+            return 0.0;
+        }
+
+        double baseFreq = getBaseFreq();
+        double maxAmplitude = 0.0;
+        double peakFreq = 0.0;
+        for (int i=0; i<spectrum.length; i++) {
+            if (spectrum[i] > maxAmplitude) {
+                maxAmplitude = spectrum[i];
+                peakFreq = baseFreq*i;
+            }
+        }
+        return peakFreq;
+    }
+
+
+    public double getBaseFreq() {
+        int sampleRate = (int)(1/TIMESTAMP_UNIT_TO_S_MULTIPLIER);
+        return (double)sampleRate/getAnalyzedDataLength();
     }
 }
