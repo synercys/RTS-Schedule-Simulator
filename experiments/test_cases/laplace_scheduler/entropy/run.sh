e19925eb502138f5ce09c3c15393f594286b5987
CY Chen <cchen140@illinois.edu>
2020-06-07 05:04:08 -0500

    [exp] Update a test script

--- experiments/test_cases/laplace_scheduler/entropy/run.sh
+++ experiments/test_cases/laplace_scheduler/entropy/run.sh
@@ -6,4 +6,7 @@ DURATION="20000"
 CASE="DURATION"
 OUTPUT_FOLDER="."
 
-$BIN_FOLDER/rtenc -i $TASKSET -o $OUTPUT_FOLDER/d$DURATION -d $DURATION -r 1000000 -v -p edf -e uapenmeanslot -c $CASE
+$BIN_FOLDER/rtenc -i $TASKSET -o $OUTPUT_FOLDER/edf_d$DURATION -d $DURATION -r 1000000 -v -p edf -e uapenmeanslot -c $CASE
+$BIN_FOLDER/rtenc -i $TASKSET -o $OUTPUT_FOLDER/ro3_d$DURATION -d $DURATION -r 1000000 -v -p reorder3 -e uapenmeanslot -c $CASE
+$BIN_FOLDER/rtenc -i $TASKSET -o $OUTPUT_FOLDER/lp10_d$DURATION -d $DURATION -r 1000000 -v -p laplace10 -e uapenmeanslot -c $CASE
+$BIN_FOLDER/rtenc -i $TASKSET -o $OUTPUT_FOLDER/lp1000_d$DURATION -d $DURATION -r 1000000 -v -p laplace1000 -e uapenmeanslot -c $CASE
